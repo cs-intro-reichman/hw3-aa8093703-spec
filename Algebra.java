09c27b3a1665d3ec4a90dda6f4ab656f2a8df58a
Ariel Wyrobnk <Ariel.Wyrobnik@post.runi.ac.il>
2025-11-21 21:45:42 +0200

    Completed assignment

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,184 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+
+		if (x2 > 0) {
+				for (int i = 0; i < x2; i++){
+				x1++;
+				} 	
+		}
+
+		else {
+			for (int i = 0; i > x2; i--){
+			x1--;
+			}
+		}
+	
+		return x1;
 	}
+
+
+
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+
+		if (x2 > 0) {
+				for (int i = 0; i < x2; i++){
+				x1--;
+				}
+		}
+
+		else {
+				for (int i = 0; i > x2; i--){
+				x1++;
+				}
+		}		
+		return x1;
 	}
+
+
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+
+		if (x1 == 0 || x2 == 0) return 0;
+
+		int holder1 = x1;
+
+		if ((x1 > 0 && x2 > 0) ) {
+
+			for (int i = 1; i < x2; i++){
+
+				for (int j = 0; j < holder1; j++){
+				x1++;
+				}
+
+			}	
+			return x1;
+		}
+
+		else if ((x1 < 0 && x2 < 0)){
+			
+
+			for (int i = 0; i < 2; i++){ // this "if" statement returns the absolute value of the negative int x1"
+										 // I wasn't sure if I was allowed to use Math.abs(), that's why I wrote the code like this
+				for (int j = 0; j > holder1; j--){
+				x1++;
+				}
+
+			}	
+
+			for (int i = -1; i > x2 ; i--){ // this one actually multiplies x1 and x2
+
+				for (int j = 0; j > holder1; j--){
+				x1++;
+				}
+			}
+		}
+
+		else {
+
+			if (x2 < 0) {
+				int temp = x1;
+				x1 = x2;
+				x2 = temp;
+				holder1 = x1;
+			}
+
+			for (int i = 1; i < x2 ; i++){ // this one actually multiplies x1 and x2
+
+				for (int j = 0; j > holder1; j--){
+				x1--;
+				}
+			}	
+		}
+			return x1;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+
+		int result;
+
+		if (n == 0) result = 1;
+		
+		else result = x;
+
+		for (int i = 1 ; i < n; i++){
+			
+			result = times(result,x);
+
+		}
+
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+
+		if (x1 == 0 || x2 == 0) return 0;
+		
+		int i = 0;
+
+		if (x1 < 0 && x2 > 0) {
+
+			while (times(i, x2) < times(x1, -1)) i++;
+
+			if (times(i, x2) == times(x1, -1)) return times(i, -1);
+		
+			return plus(times(i, -1),1);
+		}
+
+		else if (x1 > 0 && x2 < 0) {
+
+			while (times(i, times(x2, -1)) < x1) i++;
+
+			if (times(i, times(x2, -1)) == times(x1, -1)) return times(i, -1);
+		
+			return plus(times(i, -1),1);
+		}
+
+		else if (x1 > 0 && x2 > 0){
+
+			while (times(i, x2) < x1) i++;
+
+			if (times(i, x2) == x1) return i;
+		
+			return minus(i,1);
+		}
+
+		else {
+
+			while (times(i, times(x2, -1)) < times(x1, -1)) i++;
+
+			if (times(i, x2) == x1) return i;
+		
+			return minus(i,1);
+		}
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+
+		int result = minus(x1, times(x2, div(x1, x2)));
+
+		return result;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+
+		int a = 0;
+
+		while (times(a, a) < x) a++;
+
+		if (times(a, a) == x) { return a;
+			
+		}
+
+		return minus(a, 1);
+
+	
 	}	  	  
 }
